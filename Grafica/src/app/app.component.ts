a345e55240a725ccf22c64503c9f684f306ecfa7
GEstAlvAlb <alberto.a.e.g.m1997@hotmail.com>
2020-05-28 11:53:30 +0200

    grafica dividida

--- Grafica/src/app/app.component.ts
+++ Grafica/src/app/app.component.ts
@@ -51,7 +51,7 @@ export class AppComponent {
 
   view: any[] = [700, 400];
 
-  // options
+  // options tabla 1
   showXAxis = false;
   showYAxis = true;
   gradient = false;
@@ -59,6 +59,134 @@ export class AppComponent {
   showXAxisLabel = false;
   showYAxisLabel = true;
   colorScheme = {
+    domain: ['orange', 'yellow', 'red', 'purple', 'grey']
+  };
+
+  // options tabla 2
+
+  single2: any[] = [
+
+    {
+      "name": "Germany",
+      "series": [
+        {
+          "name": "2010",
+          "value": 730
+        },
+        {
+          "name": "2011",
+          "value": 894
+        },
+        {
+          "name": "1990",
+          "value": 620
+        }
+        ,
+        {
+          "name": "2012",
+          "value": 620
+        }
+        ,
+        {
+          "name": "2014",
+          "value": 620
+        }
+      ]
+    },
+
+    {
+      "name": "USA",
+      "series": [
+        {
+          "name": "2010",
+          "value": 309
+        },
+        {
+          "name": "2011",
+          "value": 311
+        },
+        {
+          "name": "1990",
+          "value": 250
+        },
+        {
+          "name": "2012",
+          "value": 620
+        }
+        ,
+        {
+          "name": "2014",
+          "value": 625
+        }
+      ]
+    },
+
+    {
+      "name": "France",
+      "series": [
+        {
+          "name": "2010",
+          "value": 500
+        },
+        {
+          "name": "2011",
+          "value": 580
+        },
+        {
+          "name": "1990",
+          "value": 580
+        },
+        {
+          "name": "2012",
+          "value": 450
+        }
+        ,
+        {
+          "name": "2014",
+          "value": 770
+        }
+      ]
+    },
+    {
+      "name": "UK",
+      "series": [
+        {
+          "name": "2010",
+          "value": 620
+        },
+        {
+          "name": "2011",
+          "value": 580
+        },
+        {
+          "name": "1990",
+          "value": 570
+        },
+        {
+          "name": "2012",
+          "value": 900
+        }
+        ,
+        {
+          "name": "2014",
+          "value": 690
+        }
+      ]
+    }
+  ]
+
+  view2: any[] = [700, 400];
+
+  showXAxis2: boolean = true;
+  showYAxis2: boolean = false;
+  gradient2: boolean = true;
+  showLegend2: boolean = false;
+  showXAxisLabel2: boolean = false;
+  xAxisLabel2: string = 'ejeX';
+  showYAxisLabel2: boolean = false;
+  yAxisLabel2: string = 'ejeY';
+
+  colorScheme2 = {
     domain: ['orange', 'yellow', 'red', 'purple', 'grey']
   };
 
